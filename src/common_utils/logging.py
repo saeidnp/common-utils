9b7f3aab7c7df0d51062f4e996512e8e65f47099
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2022-04-18 13:13:13 -0700

    bugfix

--- src/common_utils/logging.py
+++ src/common_utils/logging.py
@@ -9,5 +9,5 @@ def support_unobserve():
 def init(project_name, config, entity=None, tags=None, **kwargs):
     if entity is None:
         entity = os.environ['WANDB_ENTITY']
-    wandb.init(project=PROJECT_NAME, entity=entity,
-               config=args, tags=args.tags, **kwargs)
+    wandb.init(project=project_name, entity=entity,
+               config=config, tags=tags, **kwargs)
