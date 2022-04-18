e3051527314d4e46023a5047828b80d3c73c5d22
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2022-04-18 13:13:55 -0700

    Add num_available_cores

---
+++ src/common_utils/misc.py
@@ -0,0 +1,17 @@
+import os
+
+def num_available_cores():
+    # Copied from pytorch source code https://pytorch.org/docs/stable/_modules/torch/utils/data/dataloader.html#DataLoader
+    max_num_worker_suggest = None
+    if hasattr(os, 'sched_getaffinity'):
+        try:
+            max_num_worker_suggest = len(os.sched_getaffinity(0))
+        except Exception:
+            pass
+    if max_num_worker_suggest is None:
+        # os.cpu_count() could return Optional[int]
+        # get cpu count first and check None in order to satify mypy check
+        cpu_count = os.cpu_count()
+        if cpu_count is not None:
+            max_num_worker_suggest = cpu_count
+    return max_num_worker_suggest or 1
