944774f89641cbeb1c791c3a1b3f0fd429dfb043
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2024-04-23 11:22:05 -0700

    Add kwargs to model save function

--- common_utils/ptutils/model.py
+++ common_utils/ptutils/model.py
@@ -9,13 +9,15 @@ class ModelBase(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def save(self, path, config=None, optimizer_state_dict=None):
+    def save(self, path, config=None, optimizer_state_dict=None, **kwargs):
         to_save = {}
         to_save["state_dict"] = self.state_dict()
         if config is not None:
             to_save["config"] = config
         if optimizer_state_dict is not None:
             to_save["optimizer_state_dict"] = optimizer_state_dict
+        for k, v in kwargs.items():
+            to_save[k] = v
         torch.save(to_save, path)
 
     def load(self, path, strict=True):
