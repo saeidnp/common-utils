b9e3ee174dbfabc14137d36356e64d0f5bf17dde
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2024-09-23 14:39:51 -0700

    Add batchify

--- common_utils/misc.py
+++ common_utils/misc.py
@@ -68,3 +68,55 @@ def get_register_fn(_CLASSES):
             return _register(cls)
 
     return register_fn
+
+
+def _batchify_helper(max_batch_size, concat_fn, dim=0, batch_keys=None):
+    """
+    Decorator function that batches the output of another function based on the given parameters.
+    The decorated function should take an integer `n` as its first argument and return either a dictionary or an array/tensor.
+
+    Args:
+        max_batch_size (int or None): The maximum batch size. If `None`, no batching will be performed.
+        concat_fn (callable): A function used to concatenate the batched samples.
+        dim (int, optional): The dimension along which to concatenate the batched samples. Defaults to 0.
+        batch_keys (list or None, optional): A list of keys to batch. It should be `None` iff the decorated function returns an array/tensor.
+
+    Returns:
+        callable: The decorated function.
+
+    """
+    def decorator(func):
+        @wraps(func)
+        def wrapper(n, *args, **kwargs):
+            if max_batch_size is None or n <= max_batch_size:
+                return func(n, *args, **kwargs)
+            if batch_keys is None:
+                samples = []
+                while n > 0:
+                    current_batch_size = min(n, max_batch_size)
+                    samples.append(func(current_batch_size, *args, **kwargs))
+                    n -= current_batch_size
+                return concat_fn(samples)
+            else:
+                batched_dict = {k: [] for k in batch_keys}
+                non_batched_dict = None
+                while n > 0:
+                    current_batch_size = min(n, max_batch_size)
+                    batch = func(current_batch_size, *args, **kwargs)
+                    for k in batch_keys:
+                        batched_dict[k].append(batch[k])
+                    non_batched_dict = {k:v for k,v in batch.items() if k not in batch_keys}
+                    n -= current_batch_size
+                batched_dict = {k: concat_fn(v) for k,v in batched_dict.items()}
+                batched_dict.update(non_batched_dict)
+                return batched_dict
+        return wrapper
+    return decorator
+
+def batchify_numpy(max_batch_size, axis=0, batch_keys=None):
+    concat_fn = lambda x: np.concatenate(x, axis=axis)
+    return _batchify_helper(max_batch_size, concat_fn=concat_fn, batch_keys=batch_keys)
+
+def batchify_torch(max_batch_size, dim=0, batch_keys=None):
+    concat_fn = lambda x: torch.cat(x, dim=dim)
+    return _batchify_helper(max_batch_size, concat_fn=concat_fn, batch_keys=batch_keys)
