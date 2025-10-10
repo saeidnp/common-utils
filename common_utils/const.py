81bc05cc067c5eb8fc20ef1fe3612db0f51e37d4
Saeid Naderiparizi <saeidnp@cs.ubc.ca>
2025-10-10 07:17:00 +0000

    Remove const

--- common_utils/const.py
+++
@@ -1,3 +0,0 @@
-import os
-
-TMPDIR = "/tmp" if "EXP_TMPDIR" not in os.environ else os.environ["EXP_TMPDIR"]
--- tests/test_const.py
+++
@@ -1,5 +0,0 @@
-from common_utils.const import TMPDIR
-
-def test_tmpdir_defined():
-    assert TMPDIR is not None
-    assert isinstance(TMPDIR, str)
